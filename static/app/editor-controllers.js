0828c0876778c89998de313fc5fc4720646a6e03
Szymon Wanot <szwanot@gmail.com>
2014-11-09 14:10:24 +0100

    user controller and template change

--- static/app/editor-controllers.js
+++ static/app/editor-controllers.js
@@ -7,31 +7,30 @@ editorControllers.controller('LoginController',
 	function($scope, $location, $http, UserService, ApiService){
 		$scope.isLogin = false
 		$scope.errorMessage = undefined;
-	$scope.login = function(user){
-		var url = new ApiService;
-		url.model = 'user';
-		url.constructQuerry('name', 'eq', user.name);
-		$http.get(url.constructUrl())//todo auth in python app by post method
-		.success(function(data,status){
-			if(data.objects.length !== 0){
-				UserService.login = user.name;
-				UserService.password = user.password;
-				UserService.isLogged = true;
-				$scope.isLogin = true;
-				$location.path('/user/'+user.name);
-			}
-		})
-		.error(function(data, status){
-				$scope.errorMessage = 'login or password is incorrect';
-		});
-	}
+		$scope.login = function(user){
+			var url = new ApiService;
+			url.model = 'user';
+			url.constructQuerry('name', 'eq', user.name);
+			$http.get(url.constructUrl())//todo auth in python app by post method
+			.success(function(data,status){
+				if(data.objects.length !== 0){
+					UserService.login = user.name;
+					UserService.password = user.password;
+					UserService.isLogged = true;
+					$scope.isLogin = true;
+					$location.path('/user/'+user.name);
+				}
+			})
+			.error(function(data, status){
+					$scope.errorMessage = 'login or password is incorrect';
+			});
+		}
 
-	$scope.logOut = function(){
-		UserService.isLogged = false;
-		$scope.isLogin = false;
-		$location.path('/');
-	}
-
+		$scope.logOut = function(){
+			UserService.isLogged = false;
+			$scope.isLogin = false;
+			$location.path('/');
+		}
 }]);
 
 editorControllers.controller('HomeController',
@@ -43,37 +42,50 @@ editorControllers.controller('HomeController',
 editorControllers.controller('EditorController',
 	['$scope', '$location','NodeTool','LineTool','HandTool',
 	function($scope, $location, NodeTool, LineTool, HandTool){
-	$scope.tool = undefined;
-	$scope.graph = undefined;
-	if($scope.graph == undefined){
-		$scope.graph = {
-			nodes:[],
-			edges:[]
+		$scope.tool = undefined;
+		$scope.graph = undefined;
+		if($scope.graph == undefined){
+			$scope.graph = {
+				nodes:[],
+				edges:[]
+			}
 		}
-	}
 
-	$scope.nodeTool = function(){
-		if(!$scope.tool instanceof NodeTool)
-			$scope.tool = new NodeTool;
-	}
+		$scope.nodeTool = function(){
+			if(!$scope.tool instanceof NodeTool)
+				$scope.tool = new NodeTool;
+		}
 
-	$scope.lineTool = function(){
-		if(!$scope.tool instanceof LineTool)
-			$scope.tool = new LineTool;
-	}
+		$scope.lineTool = function(){
+			if(!$scope.tool instanceof LineTool)
+				$scope.tool = new LineTool;
+		}
 
-	$scope.handTool = function(){
-		if(!$scope.tool instanceof HandTool)
-			$scope.tool = new HandTool;
-	}
-
+		$scope.handTool = function(){
+			if(!$scope.tool instanceof HandTool)
+				$scope.tool = new HandTool;
+		}
 }]);
 
 editorControllers.controller('UserController',
-	['$scope', '$location',
-	function($scope, $location){
+	['$scope', '$location', '$http', 'UserService', 'ApiService',
+	function($scope, $location, $http, UserService){
 		$scope.userProjects = undefined;
+		$scope.loadMessage = undefined;
+		$scope.option = 'MyProjects'
+		var api = new ApiService;
+		api.model = 'scheme';
+		api.constructQuerry('user_name','eq',UserService.login);
 
+		$http.get(api.constructUrl())
+		.success(function(data, status){
+				if(data.objects.length !== 0)
+				{
+						$scope.userProjects = data.objects;
+				}else{
+						$scope.loadMessage = 'You have no projects';
+				}
+		});
 }]);
 
 editorControllers.controller('RegistrationController',
